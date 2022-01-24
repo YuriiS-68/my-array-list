6ba54cb335ed012a979cf9830927c282c3618dee
Yuvis <yurii_s@bigmir.net>
2022-01-24 13:59:52 +0200

    homework2.15 to lesson Algorithms part2

--- src/pro/sky/java/course2/homework/Main.java
+++ src/pro/sky/java/course2/homework/Main.java
@@ -5,53 +5,75 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        MyListImpl array = new MyListImpl();
-        String first = "First";
-        String second = "Second";
-        String third = "Third";
-        String fourth = "Forth";
-        String fifth = "Fifth";
-        String sixth = "Sixth";
-        String seventh = "Seventh";
-        String eighth = "Eighth";
-        String ninth = "Ninth";
-        String tenth = "Tenth";
-        String eleventh = "Eleventh";
-        String twelfth = "Twelfth";
-        String thirteenth = "Thirteenth";
-        String fourteenth = "Fourteenth";
-        String fifteenth = "Fifteenth";
-        String sixteenth = "Sixteenth";
-        String itemNew = "New";
+        IntegerListImpl array = new IntegerListImpl();
 
-        array.add(first);
-        array.add(second);
-        array.add(third);
-        //System.out.println("array = " + array);
-        //array.add(0, first);
-        //array.add(1, second);
-        //array.add(2, third);
-        //array.add(3, fourth);
-        //array.add(4, fifth);
-        //System.out.println("array = " + array);
-        System.out.println("array.set(2, itemNew) = " + array.set(2, itemNew));
-        //System.out.println("array.add(itemNew) = " + array.add(itemNew));
-        //System.out.println("array.add(2, itemNew) = " + array.add(2, itemNew));
-        //System.out.println("array = " + array);
-        //System.out.println("array.contains(itemNew) = " + array.contains(itemNew));
-        //System.out.println("array.remove(2) = " + array.remove(2));
-        //System.out.println("array.remove(second) = " + array.remove(second));
-        //System.out.println("array.indexOf(second) = " + array.indexOf(seventh));
-        System.out.println("array.lastIndexOf(third) = " + array.lastIndexOf(itemNew));
-        //System.out.println("array.get(0) = " + array.get(0));
-        array.add(thirteenth);
-        //System.out.println("array = " + array);
-        System.out.println("array.toArray() = " + Arrays.toString(array.toArray()));
-        //System.out.println("array.isEmpty() = " + array.isEmpty());
-        //array.clear();
+        System.out.println("array.contains(98_765) = " + array.contains(98_765));
+
         System.out.println("array = " + array);
-        System.out.println("array.isEmpty() = " + array.isEmpty());
-        //System.out.println("array after remove = " + array);
+
+        Integer[] arrayForTest = array.toArray();
+
+        long startBubble = System.currentTimeMillis();
+        sortBubble(arrayForTest);
+        System.out.println("Time: " + (System.currentTimeMillis() - startBubble));
+        //Time: 76913
+
+        Integer[] arrayForTest2 = array.toArray();
+
+        long startSelection = System.currentTimeMillis();
+        sortSelection(arrayForTest2);
+        System.out.println("arrayForTest2 = " + Arrays.toString(arrayForTest2));
+        System.out.println("Time: " + (System.currentTimeMillis() - startSelection));
+        //Time: 10186
+
+        Integer[] arrayForTest3 = array.toArray();
+
+        long startInsertion = System.currentTimeMillis();
+        sortInsertion(arrayForTest3);
+        System.out.println("Time: " + (System.currentTimeMillis() - startInsertion));
+        //Time: 19087
 
     }
+
+    private static void sortBubble(Integer[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]){
+                    swapElements(arr, j, j +1);
+                }
+            }
+        }
+    }
+
+    public static void sortSelection(Integer[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minElementIndex]) {
+                    minElementIndex = j;
+                }
+            }
+            swapElements(arr, i, minElementIndex);
+        }
+    }
+
+    public static void sortInsertion(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    private static void swapElements(Integer[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+
 }
